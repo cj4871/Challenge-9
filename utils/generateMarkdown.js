be2e8742737096ff60049ebe8ea5a789654dd8af
cj4871 <146256795+cj4871@users.noreply.github.com>
2024-08-07 01:39:32 -0500

    added more

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -22,11 +22,31 @@ function renderLicenseSection(license) {}
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
-
-  ## Description
-
-  ${data.}
-`;
-}
+  ###Description
+    # ${data.description}
+  ###Table of Contents
+    - [Installation](#installation)
+    - [Usage](#usage)
+    - [Contributors](#contributors)
+    - [License](#license)
+    - [Tests](#tests)
+    - [Questions](#questions)
+    - [Installation](#Installation)
+  #### ${data.contents}
+  ###Installation
+  #### ${data.installation}
+  ###Usage
+  #### ${data.usage}
+  ###Contributors
+  #### ${data.contributors}
+  ###License
+    ${console.log(renderLicenseSection(data.license))}
+    ${console.log(renderLicenseLink(data.license))}
+    ${console.log(renderLicenseBadge(data.license))}
+  #### ${data.license}
+  ###Tests
+  #### ${data.tests}
+  ###Questions
+  #### If you have any questions, contact me ${data.email}. My github profile is for my other work ${data.github}.`
 
 module.exports = generateMarkdown;
